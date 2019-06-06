412696e853034f55fd5faf4e05c1b7af135898e1
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-06 10:56:34 -0700

    Added endpoint skeleton

--- webapi-i-challenge/index.js
+++ webapi-i-challenge/index.js
@@ -7,6 +7,28 @@ const app = express();
 // use bodyparser
 app.use(express.json());
 
+// endpoints
+
+app.post('/api/users', (req,res) => {
+
+})
+
+app.get('/api/users', (req,res) => {
+
+})
+
+app.get('/api/users/:id', (req,res) => {
+
+})
+
+app.delete('/api/users/:id', (req,res) => {
+
+})
+
+app.put('/api/users/:id', (req,res) => [
+
+])
+
 // run server
 app.listen(9000, () => {
     console.log('Server is up on port 9000.');
