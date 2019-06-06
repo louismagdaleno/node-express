a8059ce5ec6f68440538c44db6fe53832bd75fc0
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-06 10:54:22 -0700

    configured express skeleton

--- webapi-i-challenge/index.js
+++ webapi-i-challenge/index.js
@@ -1,1 +1,13 @@
-// implement your API here
+// import express
+const express = require('express');
+
+// create server
+const app = express();
+
+// use bodyparser
+app.use(express.json());
+
+// run server
+app.listen(9000, () => {
+    console.log('Server is up on port 9000.');
+});
