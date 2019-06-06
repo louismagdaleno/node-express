ddc51f38b2dd04ccf88b76c897467a1299b9cb63
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-06 11:33:29 -0700

    Added axios call to backend to grab users

--- webapi-i-challenge/users/src/App.js
+++ webapi-i-challenge/users/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount(){
+    Axios.get('http://localhost:9000/api/users')
+    .then(users => {
+      this.setState({
+        users: [...JSON.parse(users)]
+      })
+    })
+    .catch(err => console.log(err));
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.users ? this.state.users.map(user => <h4>{user.name}</h4>) : <h1>Loading...</h1>}
+      </div>
+    );
+  };
+};
 
 export default App;
