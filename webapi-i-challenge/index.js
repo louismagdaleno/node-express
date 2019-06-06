666a1c5424826021972324010cf76e1dde94e7c0
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-06 11:11:43 -0700

    Completed all endpoints

--- webapi-i-challenge/index.js
+++ webapi-i-challenge/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // create server
 const app = express();
+const db = require('./data/db.js');
 
 // use bodyparser
 app.use(express.json());
@@ -10,24 +11,62 @@ app.use(express.json());
 // endpoints
 
 app.post('/api/users', (req,res) => {
+    const user = req.body;
+    db.insert(user)
+    .then(id => {
+        if (!user.name || !user.bio){
+            res.status(400).json(' {"errorMessage": "Please provide name and bio for the user."');
+        } else {
+            res.status(201).json(id);
+        }
+       
+    })
+    .catch(err => {
+        res.status(500).send(err);
+    })
 
 })
 
 app.get('/api/users', (req,res) => {
-
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
 })
 
 app.get('/api/users/:id', (req,res) => {
-
+    const { id } = req.params;
+    db.findById(id)
+    .then(user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
 })
 
 app.delete('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    db.remove(id)
+    .then(user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
 
 })
 
-app.put('/api/users/:id', (req,res) => [
-
-])
+app.put('/api/users/:id', (req,res) => {
+    const { id } = req.params;
+    const user = req.body;
+    db.update(id, user)
+    .then(user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
 
 // run server
 app.listen(9000, () => {
