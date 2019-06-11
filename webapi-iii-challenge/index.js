dcb858ea2df8b7842af02dddc3c38d7227d3b110
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-11 16:22:28 -0700

    Created MVC with node - COMPLETE

--- webapi-iii-challenge/index.js
+++ webapi-iii-challenge/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const dirPath = require('./util/path');
+const path = require('path');
 
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
-app.use('/users', userRouter);
+app.use(userRouter);
 
 // 404
 app.use((req,res,next) => {
-    res.status(404).sendFile(dirPath, 'views', '404.html');
+    res.status(404).sendFile(path.join('dirPath', 'views', '404.html'));
 });
 
 app.listen(3000, () => {
--- webapi-iii-challenge/routes/userRoutes.js
+++ webapi-iii-challenge/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dirPath = require('../util/path');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req,res,next) => {
-    res.status(200).sendfile(dirPath, 'views', 'users.html');
+    res.status(200).send('<h1>Hello from router</h1>');
+});
+
+router.get('/users', (req,res,next) => {
+    res.status(200).sendFile(path.join(dirPath, 'views', 'users.html'));
 });
 
 module.exports = router;
