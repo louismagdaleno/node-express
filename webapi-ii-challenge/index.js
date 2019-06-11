444661a672eaadc60808356f9af3f93c50af6e29
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-11 15:28:59 -0700

    Added 404 view

--- webapi-ii-challenge/index.js
+++ webapi-ii-challenge/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000, () => {
