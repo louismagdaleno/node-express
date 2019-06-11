6f993eb186e41307d7b924d8bc4f9a094c3fefce
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-11 15:13:54 -0700

    Removed posts

--- webapi-ii-challenge/index.js
+++ webapi-ii-challenge/index.js
@@ -7,6 +7,9 @@ const shopRoutes = require('./routes/shop');
 app.use(express.json());
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
